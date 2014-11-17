cbe6542a88eb2d937ede872e636c9784175630f9
geoff <valdesgeoffrey@gmail.com>
2014-11-16 18:42:26 -0600

    added morado app

--- toronjil/toronjil/urls.py
+++ toronjil/toronjil/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'toronjil.views.home', name='home'),
+    url(r'^$', 'morado.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
